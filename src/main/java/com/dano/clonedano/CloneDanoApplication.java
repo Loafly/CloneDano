9eb9a31ef2aa0c14e490abac0124c6950bed98b4
NamSeok_DT <skatjr30@naver.com>
2021-05-16 20:12:38 +0900

    init

---
+++ src/main/java/com/dano/clonedano/CloneDanoApplication.java
@@ -0,0 +1,13 @@
+package com.dano.clonedano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneDanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneDanoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dano/clonedano/CloneDanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dano.clonedano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneDanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
