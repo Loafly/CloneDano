69520c48ba04fc6d271e88bcd0d9d773951f6bf8
NamSeok_DT <skatjr30@naver.com>
2021-05-26 22:40:41 +0900

    feat : EnableJpaAuditing 추가

--- src/main/java/com/dano/clonedano/CloneDanoApplication.java
+++ src/main/java/com/dano/clonedano/CloneDanoApplication.java
@@ -2,8 +2,10 @@ package com.dano.clonedano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CloneDanoApplication {
 
     public static void main(String[] args) {
