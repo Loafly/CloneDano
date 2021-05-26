33b0cf1cad4562409f1bb662dc1af00a3a5cf9ce
NamSeok_DT <skatjr30@naver.com>
2021-05-27 00:45:33 +0900

    chore : .travis.yml 변경

--- src/main/java/com/dano/clonedano/CloneDanoApplication.java
+++ src/main/java/com/dano/clonedano/CloneDanoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CloneDanoApplication {
-    
     public static void main(String[] args) {
         SpringApplication.run(CloneDanoApplication.class, args);
     }
